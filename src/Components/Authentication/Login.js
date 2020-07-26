964ebc518d1c777155105247200582e0229ca8a3
Shivam Gupta <shivam.gupta@dal.ca>
2020-07-26 18:03:18 -0300

    changes

---
+++ src/Components/Authentication/Login.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import { login, checkIfUserAuth } from '../UserFunctions'
+
+
+
+
+
+class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: '',
+            errors: []
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+    onSubmit(e) {
+        e.preventDefault()
+        const user = {
+            email: this.state.email,
+            password: this.state.password,
+            state: 'login'
+        }
+        const errors = validate(this.state.email, this.state.password);
+        if (errors.length > 0) {
+            this.setState({ errors });
+        }
+        else {
+
+            console.log('here')
+            console.log(user)
+            checkIfUserAuth(user).then(res => {
+                if (res === 'User Verified') {
+                    console.log('done')
+                    const loginState = JSON.stringify(this.state)
+                    localStorage.setItem('loginState', loginState)
+                    localStorage.setItem('email',this.state.email)
+                    this.props.history.push(`\mfa2`)
+                    
+
+                }
+                else {
+                    console.log(res)
+                    errors.push(res)
+                    this.setState({ errors });
+                    setTimeout(() => { this.setState({ errors: [] }) }, 3000);
+                }
+
+            })
+            
+        }
+    }
+    render() {
+        const { errors } = this.state;
+        return (
+                    <div className="paneldesign">
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <h2 className="h3 mb-3 font-weight-normal text-center">
+                                Please sign in
+                            </h2>
+                            <div className="form-group ">
+                                <input type="email" className="form-control"
+                                    name="email"
+                                    placeholder="Enter email"
+                                    value={this.state.email}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <input type="password" className="form-control"
+                                    name="password"
+                                    placeholder="Enter password"
+                                    value={this.state.password}
+                                    onChange={this.onChange} />
+                            </div>
+                            <button type="submit"
+                                className="btn btn-md btn-primary btn-block" onSubmit={this.onSubmit}>
+                                Sign in
+                            </button>
+                            {errors.map(error => (
+                                <p key={error} className='error'>Error on Page: {error}</p>
+                            ))}
+                        </form>
+                    </div>
+        )
+    }
+}
+
+const validate = (email, password) => {
+    const errors = [];
+    const validEmailRegex =
+        RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    console.log(validEmailRegex.test(email))
+    if (!validEmailRegex.test(email)) {
+        errors.push("Email id is not valid")
+    }
+    if (password.length < 1) {
+        errors.push("Please enter password");
+    }
+
+    return errors;
+}
+export default Login
---
+++ src/Components/Authentication/MFA2.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react'
+import { mfa2, mfaquestion } from '../UserFunctions'
+
+class MFA2 extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            questions: [],
+            securityqa: [],
+            
+            errors: []
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
+    }
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        
+            const userDetails = JSON.parse(localStorage.getItem('loginState'))
+            localStorage.removeItem('loginState')
+            const user = {
+                
+                email: userDetails.email,
+                password: userDetails.password,
+                
+                question1: this.state.question1,
+               
+                answer1: this.state.answer1,
+              
+            }
+            mfa2(user).then(res => {
+                console.log(res)
+                if (res === 'Login successfully') {
+                    window.alert('User Logged In successfully')
+                    this.props.history.push('/landing')
+                }
+            }).catch(err => {
+                if (err) {
+                    window.alert('something wrong with the registration')
+                }
+            })
+            console.log('user logged in')
+        
+    }
+    componentDidMount() {
+        const userDetails = JSON.parse(localStorage.getItem('loginState'))
+            
+            const user = {
+                
+                email: userDetails.email,
+                
+            }
+            mfaquestion(user).then(res => {
+                console.log(res)
+                
+                    this.setState({
+                        questions: [
+                            { id: 'option1', name: res },
+                            
+                        ]
+                    });
+                
+            }).catch(err => {
+                if (err) {
+                    window.alert('error')
+                }
+            })
+
+
+        
+    }
+
+    render() {
+        const { errors } = this.state;
+        const { questions } = this.state;
+
+        let questionsList = questions.length > 0
+            && questions.map((item, i) => {
+                return (
+                    <option key={i} value={item.name}>{item.name}</option>
+                )
+            }, this);
+        return (
+            <div className="paneldesign">
+                <form noValidate onSubmit={this.onSubmit}>
+                    <h3 className="h3 mb-3 font-weight-normal">
+                        Please Select and Answer the Security question to Login
+                        </h3>
+                    <div className="form-group">
+                        <label>Question 1</label>
+                        <h5 className="form-control" defaultValue={this.state.question1}
+                            value={this.state.question1} onChange={this.onChange} name="question1">
+                            {questionsList}</h5>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control"
+                            name="answer1"
+                            placeholder="Please enter answer"
+                            value={this.state.answer1}
+                            onChange={this.onChange} />
+                    </div>
+                    
+                    <button type="submit"
+                        className="btn btn-md btn-primary btn-block" onSubmit={this.onSubmit}>
+                        Login
+                        </button>
+                    <br />
+                    {errors.map(error => (
+                        <p key={error} className='error'>Error on Page: {error}</p>
+                    ))}
+                </form>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default MFA2
